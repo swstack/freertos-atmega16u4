6b70243bd6b3f18c9411328fe764500f3d842c3a
Stephen Stack <sstack@punchthrough.com>
2016-05-07 14:55:43 -0500

    examples: add simple example of creating/running task

---
+++ examples/simple_task.c
@@ -0,0 +1,28 @@
+#include <FreeRTOS.h>
+#include <task.h>
+
+
+void simple_task(void *args) {
+  // Task runs every 100 ticks
+
+  for(;;) {
+    // Task body
+    vTaskDelay(100);  // 100 ticks
+  }
+}
+
+int main() {
+
+  xTaskCreate(
+    simple_task,                         // The task function
+    (const portCHAR *)"Simple Task",     // Name for humans
+    128,                                 // Stack size (words)
+    NULL,                                // Task parameters
+    priority,                            // Priority (1 high, 4 low)
+    NULL);
+
+  vTaskStartScheduler();
+
+  return 0;
+
+}
